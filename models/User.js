1be3c8e5a0916af58b0b89764c8ae310c6e82702
Arya urkude <aryaurkude@gmail.com>
2026-03-21 01:57:05 +0530

    Fixed missing sports array in User schema

--- models/User.js
+++ models/User.js
@@ -4,9 +4,12 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String },
+    sports: [{ type: String }], // Added this!
+    sportLevels: { type: Map, of: String }, // Added this!
     level: { type: String, default: 'Beginner' },
     reliabilityScore: { type: Number, default: 100 },
-    profileComplete: { type: Boolean, default: true }
+    matchesPlayed: { type: Number, default: 0 }, // Added this!
+    profileComplete: { type: Boolean, default: false } // Changed to false so the setup screen works properly
 }, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
